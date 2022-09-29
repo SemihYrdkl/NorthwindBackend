d117fcc401b10dc8af264762cb182a345beaee83
Semih Yurdakul <semihyurdakull@gmail.com>
2022-09-29 14:16:06 +0300

    Add project files.

---
+++ DataAccess/Concrete/EntityFramework/Context/NothwindContext.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework.Context
+{
+    public class NothwindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\v12.0;Database = Northwind; Trusted_Connection=true");
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
